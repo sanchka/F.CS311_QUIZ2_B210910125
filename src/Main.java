bb2c9aae12bc227503da291fbe33f88d1f0db7f5
Admin <Admin@DESKTOP-53KNEMD>
2023-05-03 14:22:33 +0800

    fghjk

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Course course = new Course("Software Construction");
+		course.setCourseName("Software Construction");
+//System.out.println(course1.courseName);
+		System.out.println(course.getCourseName());
+		course.setCourseName("Программ хангамжийн бүтээлт");
+		System.out.println(course.getCourseName());
+
+	}
+
+}
